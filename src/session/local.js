e459756b972d875ba862054149d9c46c9c5b50fb
Fabian Brix <brix.fabian@gmail.com>
2019-12-14 14:53:18 +0100

    #4 update session store

--- src/session/local.js
+++ src/session/local.js
@@ -1,14 +1,13 @@
 import Cache from '../cache';
 
 
-
 class LocalStore extends Cache.Local {
   constructor(...args) {
     super(...args);
   }
 
-  async set(session, {sid=this.getId(32), maxAge}) {
-    await this.cache.set(sid, this.serialize(session), maxAge/1000);
+  set(session, {sid=this.getId(32), maxAge}) {
+    this.cache.set(sid, this.serialize(session), maxAge/1000);
 
     if (this.persistent) {
       this.save();
--- src/session/redis.js
+++ src/session/redis.js
@@ -6,7 +6,6 @@ class RedisStore extends Cache.Redis {
     super(...args);
   }
 
-  // NOTE: get multiple: this.store.mget(keys)
   async set(session, {sid=this.getId(32), maxAge}) {
     await this.store.set(sid, this.serialize(session), maxAge/1000);
 
