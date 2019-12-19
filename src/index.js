fdbef793079308169d0c0b77fb69d27fc864e1d6
Fabian Brix <brix.fabian@gmail.com>
2019-12-19 15:40:31 +0100

    exports

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import * as Cache from './cache';
 import * as Session from './session';
 
 
+const exports = {
+  Lib,
+  Middleware,
+  Session,
+  Cache
+};
+
 export {
   Lib,
   Middleware,
   Session,
   Cache
 };
+
+export default exports; 
