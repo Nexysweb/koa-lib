53214af888917e21c4af0c63e80a0391efe80faf
John B <johan@nexys.ch>
2019-12-30 20:02:27 +0100

    fixed path problem (on prod)

--- src/middleware/mount.js
+++ src/middleware/mount.js
@@ -4,7 +4,7 @@ export const handler = (route, filepath) => {
   filepath = filepath || route;
 
   // todo: this will only work if the function is called from './app'.
-  const fullpath = './routes/' + filepath;
+  const fullpath = __dirname + '/routes/' + filepath;
 
   return mount(`${route}`, require(fullpath).default);
 }
