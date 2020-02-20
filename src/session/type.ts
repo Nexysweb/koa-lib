4bb5ea9f8a682b741c37bfc29eeac06619637fe2
John B <johan@nexys.ch>
2020-02-20 14:07:45 +0100

    Create type.ts

---
+++ src/session/type.ts
@@ -0,0 +1,18 @@
+export interface Profile {
+  firstName: string,
+  lastName: string
+}
+
+export interface OptionSet {
+  id: number,
+  name: string
+}
+
+export interface SessionUser {
+  profile: Profile,
+  language: OptionSet,
+  id: number,
+  email: string,
+  permissions: string[],
+  urlRedirect?:string
+}
